79613cc4eadac58f33e49f2ce8e664a49c8878d5
Simone Picciani <simone.picciani@buildo.io>
2018-09-21 18:35:23 +0200

    pretty print completed

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ const MONTHS_ITA = [
   'Dicembre',
 ];
 const DAYS_ITA = [
+  'PHAIL',
   'Lunedì',
   'Martedì',
   'Mercoledì',
@@ -48,7 +49,7 @@ const printTable = ({
 ## ${renderHeaderRange(from, to)}
 | *Giorno*         | *Cosa*                                                                                                        | *Gruppo*        | *Chi*            | *Dove Quando* |
 | ---------------- | ------------------------------------------------------------------------------------------------------------- | --------------- | ---------------- | ------------- |
-${events.map(x => renderRow(x))}
+${events.map(x => renderRow(x)).join('\n')}
 `;
 
 const renderHeaderRange = (from: Date, to: Date): string => {
@@ -68,23 +69,48 @@ const renderRow = (data: EventData): string => {
   const eventData = ` [${name}](${link})`.padEnd(111);
   return `|${evntDate}|${eventData}| ${group.padEnd(16)}| ${who.padEnd(
     16,
-  )} | ${locationTime.padEnd(13)} |
-  `;
+  )} | ${locationTime.padEnd(13)} |`;
 };
+const events: EventData[] = [
+  {
+    date: parse('20180924'),
+    name: 'Awesome Vue Vol.1',
+    link: 'https://www.meetup.com/milano-front-end/events/254444645/',
+    group: '',
+    who: 'Claudio Bisconti',
+    locationTime: 'mikamai 19:00',
+  },
 
+  {
+    date: parse('20180925'),
+    name: 'PyBirra - Sum',
+    link: 'https://www.eventbrite.com/e/registrazione-pybirra-sum-50264456342',
+    group: 'Python Milano',
+    who: 'birra',
+    locationTime: 'Baladin 19:00',
+  },
+  {
+    date: parse('20180926'),
+    name: '8080 emulator in Rust to play Space Invaders',
+    link: 'https://www.meetup.com/rust-language-milano/events/254832595/',
+    group: 'Rust Milano',
+    who: "Michele D'Amico",
+    locationTime: 'mikamai 19:00',
+  },
+  {
+    date: parse('20180927'),
+    name: 'An Evening with... Matteo Baglini',
+    link:
+      'https://www.meetup.com/Avanscoperta-Meetups-Workshops-Courses/events/253415687/',
+    group: 'Avanscoperta',
+    who: 'Matteo Baglini',
+    locationTime: 'buildo 19:00',
+  },
+];
 console.log(
   printTable({
     from: parse('20180924'),
     to: parse('20180929'),
-    events: [
-      {
-        date: parse('20180924'),
-        name: 'Awesome Vue Vol.1',
-        link: 'https://www.meetup.com/milano-front-end/events/254444645/',
-        group: 'Milano Frontend',
-        who: 'Claudio Bisconti',
-        locationTime: 'mikamai 19:00',
-      },
-    ],
+    events,
   }),
 );
