cc0b5af44bb20d96e5ac487e36a16be36c66c4e5
Simone Picciani <simone.picciani@buildo.io>
2018-09-21 20:23:13 +0200

    reordered code and aumented the width of the tabele

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ type EventData = {
   locationTime: string;
 };
 
-const printTable = ({
+const renderTable = ({
   from,
   to,
   events,
@@ -47,8 +47,8 @@ const printTable = ({
   events: EventData[];
 }): string => `
 ## ${renderHeaderRange(from, to)}
-| *Giorno*         | *Cosa*                                                                                                        | *Gruppo*        | *Chi*            | *Dove Quando* |
-| ---------------- | ------------------------------------------------------------------------------------------------------------- | --------------- | ---------------- | ------------- |
+| *Giorno*         | *Cosa*                                                                                                               | *Gruppo*        | *Chi*            | *Dove Quando* |
+| ---------------- | -------------------------------------------------------------------------------------------------------------------- | --------------- | ---------------- | ------------- |
 ${events.map(x => renderRow(x)).join('\n')}
 `;
 
@@ -66,51 +66,61 @@ const renderHeaderRange = (from: Date, to: Date): string => {
 const renderRow = (data: EventData): string => {
   const { date, group, link, locationTime, name, who } = data;
   const evntDate = ` ${DAYS_ITA[getDay(date)]} **${getDate(date)}**`.padEnd(18);
-  const eventData = ` [${name}](${link})`.padEnd(111);
+  const eventData = ` [${name}](${link})`.padEnd(118);
   return `|${evntDate}|${eventData}| ${group.padEnd(16)}| ${who.padEnd(
     16,
   )} | ${locationTime.padEnd(13)} |`;
 };
-const events: EventData[] = [
-  {
-    date: parse('20180924'),
-    name: 'Awesome Vue Vol.1',
-    link: 'https://www.meetup.com/milano-front-end/events/254444645/',
-    group: '',
-    who: 'Claudio Bisconti',
-    locationTime: 'mikamai 19:00',
-  },
 
-  {
-    date: parse('20180925'),
-    name: 'PyBirra - Sum',
-    link: 'https://www.eventbrite.com/e/registrazione-pybirra-sum-50264456342',
-    group: 'Python Milano',
-    who: 'birra',
-    locationTime: 'Baladin 19:00',
-  },
-  {
-    date: parse('20180926'),
-    name: '8080 emulator in Rust to play Space Invaders',
-    link: 'https://www.meetup.com/rust-language-milano/events/254832595/',
-    group: 'Rust Milano',
-    who: "Michele D'Amico",
-    locationTime: 'mikamai 19:00',
-  },
-  {
-    date: parse('20180927'),
-    name: 'An Evening with... Matteo Baglini',
-    link:
-      'https://www.meetup.com/Avanscoperta-Meetups-Workshops-Courses/events/253415687/',
-    group: 'Avanscoperta',
-    who: 'Matteo Baglini',
-    locationTime: 'buildo 19:00',
-  },
-];
-console.log(
-  printTable({
+function printTable(table: string): void {
+  console.log(table);
+}
+
+function main(): void {
+  const events: EventData[] = [
+    {
+      date: parse('20180924'),
+      name: 'Awesome Vue Vol.1',
+      link: 'https://www.meetup.com/milano-front-end/events/254444645/',
+      group: 'Milano Frontend',
+      who: 'Claudio Bisconti',
+      locationTime: 'mikamai 19:00',
+    },
+    {
+      date: parse('20180925'),
+      name: 'PyBirra - Sum',
+      link:
+        'https://www.eventbrite.com/e/registrazione-pybirra-sum-50264456342',
+      group: 'Python Milano',
+      who: 'birra',
+      locationTime: 'Baladin 19:00',
+    },
+    {
+      date: parse('20180926'),
+      name: '8080 emulator in Rust to play Space Invaders',
+      link: 'https://www.meetup.com/rust-language-milano/events/254832595/',
+      group: 'Rust Milano',
+      who: "Michele D'Amico",
+      locationTime: 'mikamai 19:00',
+    },
+    {
+      date: parse('20180927'),
+      name: 'An Evening with... Matteo Baglini',
+      link:
+        'https://www.meetup.com/Avanscoperta-Meetups-Workshops-Courses/events/253415687/',
+      group: 'Avanscoperta',
+      who: 'Matteo Baglini',
+      locationTime: 'buildo 19:00',
+    },
+  ];
+
+  const table = renderTable({
     from: parse('20180924'),
     to: parse('20180929'),
     events,
-  }),
-);
+  });
+
+  printTable(table);
+}
+
+main();
